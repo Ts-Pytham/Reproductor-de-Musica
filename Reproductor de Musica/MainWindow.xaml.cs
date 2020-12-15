29d755c319b91ec140e75576ac7e16af045ab40b
Ts-Pytham <43942761+Ts-Pytham@users.noreply.github.com>
2020-12-15 18:13:39 -0500

    1.1
    - Se agregó animaciones a los botones y funcionamiento.
    - Se mejoró la interfaz.

--- Reproductor de Musica/MainWindow.xaml.cs
+++ Reproductor de Musica/MainWindow.xaml.cs
@@ -75,46 +75,107 @@ namespace Reproductor_de_Musica
         {
             TextBlock textBlock = (TextBlock)sender;
 
-            textBlock.Foreground = Brushes.Gray;
-
-            Rectangle_Barra.Visibility = Visibility.Visible;
-
-            using (var fd = new WinForms.OpenFileDialog())
+            textBlock.Foreground = Brushes.Gray; 
+            /* El if comprueba cual textblock fue el que se presionó, 
+             * solo sirve para ocultar/mostrar el rectángulo y llama a la función para 
+             * agregar la música, es decir, si entra en el else es porque el usuario ingresa 
+             * una playlist. */
+            if (textBlock.Name == "TextBlock_Add") 
             {
-                fd.Filter = "Image Files(*.MP3)|*.MP3";
+                Rectangle_Barra.Visibility = Visibility.Visible;
+                Rectangle_Barra1.Visibility = Visibility.Hidden;
 
-                fd.FilterIndex = 1;
-
-                if (fd.ShowDialog() == WinForms.DialogResult.OK)
+                using (var fd = new WinForms.OpenFileDialog())
                 {
+                    fd.Filter = "Image Files(*.MP3; *.Webm)|*.MP3; *.Webm";
 
-                    Name_Music.Text = fd.SafeFileName;
-                    
-                    mediaPlayer.Open(new Uri(fd.FileName));
-                    
+                    fd.FilterIndex = 1;
 
-                    Image img = new Image { Source = new BitmapImage(new Uri(@"pack://application:,,,/IMG/pausa.png")) };
-                    Button_Reproductor.Content = img;
-                    IsPaused = false;
-                    
-                    DispatcherTimer timer = new DispatcherTimer
+                    if (fd.ShowDialog() == WinForms.DialogResult.OK)
                     {
-                        Interval = TimeSpan.FromSeconds(1)
-                    };
-                    timer.Tick += Timer_Tick;
-                    timer.Start();
-                    
-                    
-                    mediaPlayer.Volume = 0.5;
-                    Slider_Volumen.Value = mediaPlayer.Volume;
 
-                    mediaPlayer.MediaOpened += MediaPlayer_MediaOpened;
+                        Name_Music.Text = fd.SafeFileName;
 
-                    ListBox.Items.Add(fd.SafeFileName);
-                    
-                    URLS.Add(fd.FileName);
+                        mediaPlayer.Open(new Uri(fd.FileName));
+
+
+                        Image img = new Image { Source = new BitmapImage(new Uri(@"pack://application:,,,/IMG/pausa.png")) };
+                        Button_Reproductor.Content = img;
+                        IsPaused = false;
+
+                        DispatcherTimer timer = new DispatcherTimer
+                        {
+                            Interval = TimeSpan.FromSeconds(1)
+                        };
+                        timer.Tick += Timer_Tick;
+                        timer.Start();
+
+
+                        mediaPlayer.Volume = 0.5;
+                        Slider_Volumen.Value = mediaPlayer.Volume;
+
+                        mediaPlayer.MediaOpened += MediaPlayer_MediaOpened;
+
+                        ListBox.Items.Add(fd.SafeFileName);
+
+                        URLS.Add(fd.FileName);
+
+                        ListBox.SelectedIndex = 0;
+                    }
                 }
             }
+            else 
+            { 
+                Rectangle_Barra1.Visibility = Visibility.Visible;
+                Rectangle_Barra.Visibility = Visibility.Hidden;
+
+                using (var fd = new WinForms.OpenFileDialog())
+                {
+                    fd.Multiselect = true;
+
+                    fd.Filter = "Image Files(*.MP3; *.Webm)|*.MP3; *.Webm";
+
+                    fd.FilterIndex = 1;
+
+                    if (fd.ShowDialog() == WinForms.DialogResult.OK)
+                    {
+
+                        Name_Music.Text = fd.SafeFileName;
+
+                        mediaPlayer.Open(new Uri(fd.FileName));
+
+
+                        Image img = new Image { Source = new BitmapImage(new Uri(@"pack://application:,,,/IMG/pausa.png")) };
+                        Button_Reproductor.Content = img;
+                        IsPaused = false;
+
+                        DispatcherTimer timer = new DispatcherTimer
+                        {
+                            Interval = TimeSpan.FromSeconds(1)
+                        };
+                        timer.Tick += Timer_Tick;
+                        timer.Start();
+
+
+                        mediaPlayer.Volume = 0.5;
+                        Slider_Volumen.Value = mediaPlayer.Volume;
+
+                        mediaPlayer.MediaOpened += MediaPlayer_MediaOpened;
+
+                        foreach(var dato in fd.SafeFileNames)
+                        {
+                            
+                            ListBox.Items.Add(dato);
+                        }  
+                        foreach(var dato in fd.FileNames)
+                        {
+                            URLS.Add(dato);
+                        }
+                    }
+                }
+            }
+
+            
         }
 
         private void MediaPlayer_MediaOpened(object sender, EventArgs e)
@@ -140,6 +201,8 @@ namespace Reproductor_de_Musica
             TextBlock textBlock = (TextBlock)sender;
 
             textBlock.Foreground = Brushes.White;
+
+            
         }
 
         private void TextBlock_Add_MouseLeftButtonUp(object sender, MouseButtonEventArgs e) => TextBlock_Add_MouseEnter(sender, e);
@@ -153,7 +216,8 @@ namespace Reproductor_de_Musica
         {
             mediaPlayer.Open(new Uri(URLS[ListBox.SelectedIndex]));
             Name_Music.Text = ListBox.SelectedItem.ToString();
-
+            IsPaused = true;
+            Button_Pause_Click(sender, e);
             mediaPlayer.Play();
         }
 
@@ -170,5 +234,103 @@ namespace Reproductor_de_Musica
             int pos = Convert.ToInt32(Slider_Carga.Value);
             mediaPlayer.Position = new TimeSpan(0, 0, 0, pos, 0);
         }
+
+
+
+
+
+        /*     EVENTOS POR DEFECTOS DE LAS IMAGENES    */
+
+        private void Image_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Image img = (Image)sender;
+            if(img.Name == "Siguiente")
+              img.Source = new BitmapImage(new Uri(@"pack://application:,,,/IMG/Models_Siguiente/hover_siguiente.png"));
+            else if(img.Name == "Anterior")
+                img.Source = new BitmapImage(new Uri(@"pack://application:,,,/IMG/Models_Anterior/hover_anterior.png"));
+            else if(img.Name == "Repetir")
+                img.Source = new BitmapImage(new Uri(@"pack://application:,,,/IMG/Repetir/hover_actualizar.png"));
+        }
+
+        private void Image_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Image img = (Image)sender;
+            if (img.Name == "Siguiente")
+                img.Source = new BitmapImage(new Uri(@"pack://application:,,,/IMG/Models_Siguiente/siguiente.png"));
+            else if (img.Name == "Anterior")
+                img.Source = new BitmapImage(new Uri(@"pack://application:,,,/IMG/Models_Anterior/anterior.png"));
+            else if (img.Name == "Repetir")
+                img.Source = new BitmapImage(new Uri(@"pack://application:,,,/IMG/Repetir/actualizar.png"));
+        }
+
+        private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Image img = (Image)sender;
+            if (img.Name == "Siguiente")
+            {
+                img.Source = new BitmapImage(new Uri(@"pack://application:,,,/IMG/Models_Siguiente/enter_siguiente.png"));
+                if (ListBox.Items.Count == 1)
+                {
+                    mediaPlayer.Open(new Uri(URLS[0]));
+
+                }
+                else if(ListBox.SelectedIndex == ListBox.Items.Count - 1)
+                {
+                    mediaPlayer.Open(new Uri(URLS[0]));
+                    Name_Music.Text = ListBox.Items[0].ToString();
+                    ListBox.SelectedIndex = 0;
+                }
+                else
+                {
+                    mediaPlayer.Open(new Uri(URLS[ListBox.SelectedIndex + 1]));
+                    Name_Music.Text = ListBox.Items[ListBox.SelectedIndex + 1].ToString();
+                    ListBox.SelectedIndex += 1;
+                }
+
+                IsPaused = true;
+                Button_Pause_Click(sender, e);
+                mediaPlayer.Play();
+            }
+            else if (img.Name == "Anterior")
+            {
+                img.Source = new BitmapImage(new Uri(@"pack://application:,,,/IMG/Models_Anterior/enter_anterior.png"));
+                if (ListBox.Items.Count == 1)
+                {
+                    mediaPlayer.Open(new Uri(URLS[0]));
+                    
+                    
+                }
+                else if(ListBox.SelectedIndex != 0)
+                {
+
+                    mediaPlayer.Open(new Uri(URLS[ListBox.SelectedIndex - 1]));
+                    Name_Music.Text = ListBox.Items[ListBox.SelectedIndex - 1].ToString();
+                    ListBox.SelectedIndex -= 1;
+                }
+                IsPaused = true;
+                Button_Pause_Click(sender, e);
+                mediaPlayer.Play();
+            }
+            else if(img.Name == "Repetir")
+            {
+                img.Source = new BitmapImage(new Uri(@"pack://application:,,,/IMG/Repetir/click_actualizar.png"));
+                mediaPlayer.Open(new Uri(URLS[ListBox.SelectedIndex]));
+
+                IsPaused = true;
+                Button_Pause_Click(sender, e);
+                mediaPlayer.Play();
+            }
+        }
+
+        private void Image_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Image img = (Image)sender;
+            if (img.Name == "Siguiente")
+                img.Source = new BitmapImage(new Uri(@"pack://application:,,,/IMG/Models_Siguiente/siguiente.png"));
+            else if (img.Name == "Anterior")
+                img.Source = new BitmapImage(new Uri(@"pack://application:,,,/IMG/Models_Anterior/anterior.png"));
+        }
+
+        
     }
 }
