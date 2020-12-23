2d8a3fd6b7a3939a8b8e89510b5bb9a8c606316a
Ts-Pytham <43942761+Ts-Pytham@users.noreply.github.com>
2020-12-23 16:27:01 -0500

    1.2.0

--- Reproductor de Musica/obj/Debug/Reproductor de Musica_Content.g.i.cs
+++ Reproductor de Musica/obj/Debug/Reproductor de Musica_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("pausa.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icon.png")]
 
 
